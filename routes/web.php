47ec224f61d867634fc2c473c4669338cd1e128b
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2024-01-06 16:42:21 +0900

    修正したヒアドキュメント

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt;color:#999;}
+h1{ font-sixe:100pt;text-align:right;color:#eee
+    margin:-40px 0px -50px 0px;}
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これはサンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+Route::get('hello', function ()use ($html) {
+    return $html;
 });
 Route::get('/', function () {
     return view('welcome');
